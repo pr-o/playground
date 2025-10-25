4c1b80172f7f6f4e69555521855559cdceb9fc49
pr-o <sunghahh.git@gmail.com>
2025-10-25 15:54:16 +0900

    chore: update URLs in personal-section also

--- src/components/apps/youtube/home-sidebar/personal-section.tsx
+++ src/components/apps/youtube/home-sidebar/personal-section.tsx
@@ -3,43 +3,61 @@
 import {
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarGroupLabel,
 } from '@/components/ui/sidebar';
+
 import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const baseURL = '/apps/youtube';
 
 const items = [
   {
     title: 'History',
-    url: '/playlists/history',
+    url: `${baseURL}/playlists/history`,
     icon: HistoryIcon,
     auth: true,
   },
   {
     title: 'Liked videos',
-    url: '/playlists/liked',
+    url: `${baseURL}/playlists/liked`,
     icon: ThumbsUpIcon,
     auth: true,
   },
   {
     title: 'All playlists',
-    url: '/playlists',
+    url: `${baseURL}/playlists`,
     icon: ListVideoIcon,
     auth: true,
   },
 ];
 
 export const PersonalSection = () => {
+  const pathname = usePathname();
+
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>You</SidebarGroupLabel>
       <SidebarGroupContent>
+        <SidebarGroupLabel>Your</SidebarGroupLabel>
         <SidebarMenu>
           {items.map((item) => (
-            <SidebarMenuItem key={item.title}></SidebarMenuItem>
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton
+                tooltip={item.title}
+                asChild
+                isActive={pathname === item.url}
+                onClick={(e) => {}}
+              >
+                <Link prefetch href={item.url} className="flex items-center gap-4">
+                  <item.icon />
+                  <span className="text-sm">{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
           ))}
         </SidebarMenu>
       </SidebarGroupContent>
